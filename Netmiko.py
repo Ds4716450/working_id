c7242da91736394b0d1811892b43e087eedd48ea
Devendra Singh <144020236+Ds4716450@users.noreply.github.com>
2026-02-03 17:20:24 +0530

    Netmiko file attach
    this is a netmiko code ,its's connect VS code and VM (virtual machine)

---
+++ Netmiko.py
@@ -0,0 +1,11 @@
+from netmiko import ConnectHandler
+net_connect=ConnectHandler(
+    device_type='linux',
+    host='10.0.4.253',
+    username='vcti',
+    password='inD@18us'
+)
+output=net_connect.send_command(
+    'cd Dev72 && touch de.txt && ls'
+)
+print(output)
